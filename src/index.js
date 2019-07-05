a0662e554c6e682560df07a2299dfcb78c9cebec
Herman Rivas <hrivas5726@aol.com>
2019-07-05 09:32:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+
+const Navbar = () =>
+  <div>
+    <NavLink to="/" exact style={link} activerStyle={{background: 'darkblue'}}>Home</NavLink>
+    <NavLink to="/about" exact style={link} activerStyle={{background: 'darkblue'}}>About</NavLink>
+    <NavLink to="/login" exact style={link} activerStyle={{background: 'darkblue'}}>Login</NavLink>
+  </div>
+
 
 const Home = () => {
   return (
@@ -9,7 +28,40 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar /> <br/>
+      <Route exact path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
